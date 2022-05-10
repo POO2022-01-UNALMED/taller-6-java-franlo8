551b783d2fe7869e00c3ae76255db12299f1a5ac
franlo8 <fmicolta@unal.edu.co>
2022-05-10 00:00:43 -0500

    commit xd

--- src/main/java/Vehiculos/automovil.java
+++ src/main/java/Vehiculos/automovil.java
@@ -1,10 +1,24 @@
 package Vehiculos;
 
-public class Automovil {
+public class Automovil extends Vehiculo{
    int puestos;
    public static int puertas= 4;
    public static int velocidadMaxima = 100;
    public static String traccion = "FWD";
+
+    public Automovil (String placa, int velocidadMaxima, int puertas, String nombre, int precio, int peso, String traccion, Fabricante fabricante, int CantidadVehiculos, int puestos) {
+        setPlaca(placa);
+        setVelocidadMaxima(velocidadMaxima);
+        setPuertas(puertas);
+        setNombre(nombre);
+        setPrecio(precio);
+        setPeso(peso);
+        setTraccion(traccion);
+        setFabricante(fabricante);
+        setCantidadVehiculos(CantidadVehiculos);
+        this.puestos= puestos;
+    }
+
 }
 
 public class Vehiculo {
@@ -92,11 +106,35 @@ public class Vehiculo {
 }
     public class Pais {
        private String nombre;
-}
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+    }
 
 public class Fabricante {
      private String nombre;
      private Pais pais;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
 }
 
 public class Camion extends Vehiculo {
@@ -104,13 +142,38 @@ public class Camion extends Vehiculo {
      public static int puertas = 2;
      public static int velocidadMaxima = 80;
      public static String traccion = "4X2";
+
+     public Camion (String placa, int velocidadMaxima, int puertas, String nombre, int precio, int peso, String traccion, Fabricante fabricante, int CantidadVehiculos, int ejes) {
+         setPlaca(placa);
+         setVelocidadMaxima(velocidadMaxima);
+         setPuertas(puertas);
+         setNombre(nombre);
+         setPrecio(precio);
+         setPeso(peso);
+         setTraccion(traccion);
+         setFabricante(fabricante);
+         setCantidadVehiculos(CantidadVehiculos);
+         this.ejes= ejes;
+     }
 }
 
 public class Camioneta extends Vehiculo {
        private boolean volco;
        public static int velocidadMaxima = 90;
        public static String traccion = "4X4";
+
+    public Camioneta (String placa, int velocidadMaxima, int puertas, String nombre, int precio, int peso, String traccion, Fabricante fabricante, int CantidadVehiculos, boolean volco) {
+        setPlaca(placa);
+        setVelocidadMaxima(velocidadMaxima);
+        setPuertas(puertas);
+        setNombre(nombre);
+        setPrecio(precio);
+        setPeso(peso);
+        setTraccion(traccion);
+        setFabricante(fabricante);
+        setCantidadVehiculos(CantidadVehiculos);
+        this.volco= volco;
+    }
 }
 
-//si viste como era?
-//otra vez entonces
+
